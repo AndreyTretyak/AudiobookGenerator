672e9c2bfa2cc309af4fd8fc9b7474d2addcedc3
Andrii Tretiak <andrey.o.tretyak@gmail.com>
2024-10-27 04:17:59 -0700

    more splitting

--- AudiobookGenerator/Program.cs
+++ AudiobookGenerator/Program.cs
@@ -19,7 +19,7 @@ public readonly record struct BookChapter(string FileName, string Name, string C
 
 public readonly record struct BookImage(string FileName, byte[] Content);
 
-public readonly record struct EpubBook(
+public readonly record struct Book(
     string FileName,
     string Title,
     string Description,
@@ -30,7 +30,7 @@ public readonly record struct EpubBook(
 
 public interface IEpubBookParser
 {
-    Task<EpubBook> ParseAsync(FileInfo fileInfo, CancellationToken token);
+    Task<Book> ParseAsync(FileInfo fileInfo, CancellationToken token);
 }
 
 public interface IHtmlConverter
@@ -57,6 +57,101 @@ public interface IAudioConverter
     Task<Stream> CreateM4bAsync(IEnumerable<Stream> aacChapters, CancellationToken cancellationToken);
 
     Task AddImagesAndTagsToM4bAsync(CancellationToken cancellationToken);
+}
+
+internal class FfmpegAudioCopnverter : IInitializer
+{
+    public Task AddImagesAndTagsToM4bAsync(FileInfo m4bFile, Book bookInfo, CancellationToken cancellationToken)
+    {
+        static Picture ByteToPicture(byte[] bytes) => new Picture(new ByteVector(bytes));
+
+        var file = TagLib.File.Create(m4bFile.FullName);
+
+        IPicture? coverImage = null;
+        if (bookInfo.CoverImage != null)
+        {
+            coverImage = ByteToPicture(bookInfo.CoverImage);
+            coverImage.Type = TagLib.PictureType.FrontCover;
+        }
+
+        file.Tag.Title = bookInfo.Title;
+        file.Tag.TitleSort = bookInfo.Title;
+        file.Tag.Album = bookInfo.Title;
+        file.Tag.Comment = bookInfo.Description;
+        file.Tag.Performers = [.. bookInfo.AuthorList];
+
+        var allImages = bookInfo.Images.Select(i => ByteToPicture(i.Content));
+        file.Tag.Pictures = coverImage != null ? [coverImage, .. allImages] : allImages.ToArray();
+
+        file.Save();
+
+        return Task.CompletedTask;
+    }
+
+    public async Task<FileInfo> ConvertWavToAacAsync(Stream wavStream, FileInfo outputFile, CancellationToken cancellationToken)
+    {
+        await FFMpegArguments
+            .FromPipeInput(new StreamPipeSource(wavStream))
+            .OutputToFile(outputFile.FullName, true, options => options.WithAudioCodec(AudioCodec.Aac))
+            .ProcessAsynchronously();
+
+        return outputFile;
+    }
+
+    public async Task<FileInfo> CreateM4bAsync(IEnumerable<FileInfo> aacChapters, FileInfo outputFile, CancellationToken cancellationToken)
+    {
+        var files = aacChapters.Select(f => f.FullName).ToArray();
+
+        if (outputFile.Directory == null) 
+        {
+            throw new InvalidOperationException($"Output directory for {outputFile} not found.");
+        }
+        var outputDir = outputFile.Directory.FullName;
+
+        var chaptersFile = Path.Combine(outputDir, "chapters.txt");
+        using (StreamWriter stream = new StreamWriter(chaptersFile))
+        {
+            stream.WriteLine(";FFMETADATA1");
+
+            long start = 0;
+            foreach (var file in files)
+            {
+                var mediaInfo = await FFProbe.AnalyseAsync(file, cancellationToken: cancellationToken);
+                var end = start + (long)mediaInfo.Duration.TotalMilliseconds;
+
+                stream.WriteLine("[CHAPTER]");
+                stream.WriteLine("TIMEBASE=1/1000");
+                stream.WriteLine($"START={start}");
+                stream.WriteLine($"END={end}");
+                stream.WriteLine($"title={Path.GetFileNameWithoutExtension(file)}");
+                stream.WriteLine("");
+
+                start = end + 1;
+            }
+        }
+
+        var output = Path.Join(outputDir, outputFile.Name);
+
+        await FFMpegArguments
+            .FromConcatInput(files)
+            .AddFileInput(chaptersFile)
+            .OutputToFile(output, true)
+            .ProcessAsynchronously();
+
+        return outputFile;
+    }
+
+    public Task InitializeAsync(CancellationToken cancellationToken)
+    {
+        Process process = new();
+        process.StartInfo.FileName = "cmd.exe";
+        process.StartInfo.Arguments = "/c winget install ffmpeg --accept-source-agreements";
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.CreateNoWindow = true;
+        process.Start();
+        return process.WaitForExitAsync(cancellationToken);
+    }
 }
 
 internal class LocalAudioSynthesizer : IAudioSynthesizer
@@ -89,6 +184,50 @@ internal class LocalAudioSynthesizer : IAudioSynthesizer
             return Task.FromResult(Stream.Null);
         }
     }
+
+    //static async Task Main(string[] args)
+    //{
+    //    var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
+
+    //    var audioConfig = AudioConfig.FromWavFileOutput("output.wav");
+
+    //    using var synthesizer = new SpeechSynthesizer(config, audioConfig);
+    //    var result = await synthesizer.SpeakTextAsync("Hello, world!");
+
+    //    if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+    //    {
+    //        Console.WriteLine("Speech synthesized to file successfully.");
+    //    }
+    //    else if (result.Reason == ResultReason.Canceled)
+    //    {
+    //        var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
+    //        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+    //        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+    //    }
+    //}
+
+    //public static async Task Main(string[] args)
+    //{
+    //    var playwright = await Playwright.CreateAsync();
+    //    var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+    //    {
+    //        Channel = "msedge", // Use the Edge browser
+    //        Headless = false
+    //    });
+    //    var page = await browser.NewPageAsync();
+    //    await page.GotoAsync("https://example.com");
+
+    //    // Inject JavaScript to use the Web Speech API for TTS
+    //    await page.EvaluateAsync(@"() => {
+    //        const utterance = new SpeechSynthesisUtterance('Hello, world!');
+    //        speechSynthesis.speak(utterance);
+    //    }");
+
+    //    // Wait for the speech to finish
+    //    await Task.Delay(5000);
+
+    //    await browser.CloseAsync();
+    //}
 }
 
 internal class PlaywrightHtmlConverter : IHtmlConverter, IInitializer, IDisposable
@@ -135,14 +274,14 @@ internal class VersOneEpubBookParser(IHtmlConverter converter) : IEpubBookParser
 {
     public EpubReaderOptions EpubReaderOptions { get; set; } = new EpubReaderOptions();
 
-    public async Task<EpubBook> ParseAsync(FileInfo fileInfo, CancellationToken cancellationToken)
+    public async Task<Book> ParseAsync(FileInfo fileInfo, CancellationToken cancellationToken)
     {
         var stream = fileInfo.OpenRead();
         var book = EpubReader.ReadBook(stream, EpubReaderOptions);
         var convertTask = book.ReadingOrder.Select(chapter => ChapterToPlainTextAsync(chapter, cancellationToken));
         var plainTextChapters = await Task.WhenAll(convertTask).ConfigureAwait(false);
-        return new EpubBook(
-            fileInfo.Name,
+        return new Book(
+            Path.GetFileNameWithoutExtension(fileInfo.Name),
             book.Title,
             book.Description ?? string.Empty,
             book.AuthorList,
@@ -178,36 +317,13 @@ internal class Program
         await RunAsync(@"F:\Downloads\Long Chills and Case Dough by Brandon Sanderson.epub", @"E:\Downloads\", "en-US");
     }
 
-    private static Task InstallDependenciesAsync(CancellationToken cancellationToken) 
-    {
-        Microsoft.Playwright.Program.Main(["install"]);
-
-        Process process = new Process();
-        process.StartInfo.FileName = "cmd.exe";
-        process.StartInfo.Arguments = "/c winget install ffmpeg --accept-source-agreements";
-        process.StartInfo.RedirectStandardOutput = true;
-        process.StartInfo.UseShellExecute = false;
-        process.StartInfo.CreateNoWindow = true;
-
-        process.Start();
-
-        return process.WaitForExitAsync(cancellationToken);
-    }
-
-    /// <summary>
-    /// Generate .wav files from .txt
-    /// </summary>
-    /// <param name="input">Directory that contains .epub files.</param>
-    /// <param name="output">Directory to put corresponding .wav files.</param>
-    /// <param name="language">Language of the voice to use. Defaults to English.</param>
     static async Task RunAsync(string input, string output, string language = "en-US")
     {
         CancellationToken cancellationToken = default;
-
-        await InstallDependenciesAsync(cancellationToken);
-
         var htmlConverter = new PlaywrightHtmlConverter();
+        var audioConverter = new FfmpegAudioCopnverter();
         await htmlConverter.InitializeAsync(cancellationToken);
+        await audioConverter.InitializeAsync(cancellationToken);
         var bookParser = new VersOneEpubBookParser(htmlConverter);
         var synthesizer = new LocalAudioSynthesizer();
         var book = await bookParser.ParseAsync(new FileInfo(input), cancellationToken);
@@ -220,7 +336,8 @@ internal class Program
         {
             var voice = synthesizer.GetVoices().Single(v => v.Gender == VoiceGender.Female && v.Culture.Name == language);
             using var stream = await synthesizer.SynthesizeWavFromTextAsync(chapter.Name, chapter.Content, voice, cancellationToken);
-            var result = await ConvertWavToAccAsync(stream, Path.Join(aacDir.FullName, $"{chapter.FileName}.aac"));
+            var chapterAacOutput = new FileInfo(Path.Join(aacDir.FullName, $"{chapter.FileName}.aac"));
+            await audioConverter.ConvertWavToAacAsync(stream, chapterAacOutput, cancellationToken);
         }
 
         foreach (var image in book.Images)
@@ -232,90 +349,17 @@ internal class Program
 
         Log("Joining", ConsoleColor.Yellow);
         var bookFileName = $"{book.FileName}.m4b";
-        await ConcatAccToM4bAsync(aacDir, outDir, bookFileName);
+        var bookPath = Path.Combine(outDir.FullName, bookFileName);
+        var bookFile = new FileInfo(bookPath);
+        await audioConverter.CreateM4bAsync(aacDir.GetFiles(), bookFile, cancellationToken);
         Log("Done joining", ConsoleColor.Green);
 
-        if (book.CoverImage != null)
-        {
-            Log($"Adding cover image", ConsoleColor.Yellow);
-            await AddCoverImageAndTagsAsync(Path.Combine(outDir.FullName, bookFileName), book);
-            Log("Done adding cover", ConsoleColor.Green);
-        }
+        Log($"Adding cover image", ConsoleColor.Yellow);
+        await audioConverter.AddImagesAndTagsToM4bAsync(bookFile, book, cancellationToken);
+        Log("Done adding cover", ConsoleColor.Green);
 
         Log("Done", ConsoleColor.Green);
         Console.ReadLine();
-    }
-
-    private static Task<bool> ConvertWavToAccAsync(Stream wavStream, string output)
-    {
-        return FFMpegArguments
-            .FromPipeInput(new StreamPipeSource(wavStream))
-            .OutputToFile(output, true, options => options.WithAudioCodec(AudioCodec.Aac))
-            .ProcessAsynchronously();
-    }
-
-    private static async Task<bool> ConcatAccToM4bAsync(DirectoryInfo inputDir, DirectoryInfo outputDir, string outputFileName)
-    {
-        var files = inputDir.GetFiles().Select(f => f.FullName).ToArray();
-
-
-        var chaptersFile = Path.Combine(outputDir.FullName, "chapters.txt");
-        using (StreamWriter stream = new StreamWriter(chaptersFile))
-        {
-            stream.WriteLine(";FFMETADATA1");
-
-            long start = 0;
-            foreach (var file in files)
-            {
-                var mediaInfo = await FFProbe.AnalyseAsync(file);
-                var end = start + (long)mediaInfo.Duration.TotalMilliseconds;
-
-                stream.WriteLine("[CHAPTER]");
-                stream.WriteLine("TIMEBASE=1/1000");
-                stream.WriteLine($"START={start}");
-                stream.WriteLine($"END={end}");
-                stream.WriteLine($"title={Path.GetFileNameWithoutExtension(file)}");
-                stream.WriteLine("");
-
-                start = end + 1;
-            }
-        }
-
-        var output = Path.Join(outputDir.FullName, outputFileName);
-
-
-        return await FFMpegArguments
-            .FromConcatInput(files)
-            .AddFileInput(chaptersFile)
-            .OutputToFile(output, true)
-            .ProcessAsynchronously();
-    }
-
-    private static Task<bool> AddCoverImageAndTagsAsync(string filePath, EpubBook book) 
-    {
-        static Picture ByteToPicture(byte[] bytes) => new TagLib.Picture(new ByteVector(bytes));
-
-        var file = TagLib.File.Create(filePath);
-
-        IPicture? coverImage = null;
-        if (book.CoverImage != null) 
-        {
-            coverImage = ByteToPicture(book.CoverImage);
-            coverImage.Type = TagLib.PictureType.FrontCover;
-        } 
-
-        file.Tag.Title = book.Title;
-        file.Tag.TitleSort = book.Title;
-        file.Tag.Album = book.Title;
-        file.Tag.Comment = book.Description;
-        file.Tag.Performers = [.. book.AuthorList];
-
-        var allImages = book.Images.Select(i => ByteToPicture(i.Content));
-        file.Tag.Pictures = coverImage != null ? [coverImage, ..allImages] : allImages.ToArray();
-
-        file.Save();
-
-        return Task.FromResult(true);
     }
 
     internal static void Log(string message, ConsoleColor color)
@@ -325,48 +369,4 @@ internal class Program
         Console.WriteLine(message);
         Console.ForegroundColor = currentColor;
     }
-
-    //static async Task Main(string[] args)
-    //{
-    //    var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
-
-    //    var audioConfig = AudioConfig.FromWavFileOutput("output.wav");
-
-    //    using var synthesizer = new SpeechSynthesizer(config, audioConfig);
-    //    var result = await synthesizer.SpeakTextAsync("Hello, world!");
-
-    //    if (result.Reason == ResultReason.SynthesizingAudioCompleted)
-    //    {
-    //        Console.WriteLine("Speech synthesized to file successfully.");
-    //    }
-    //    else if (result.Reason == ResultReason.Canceled)
-    //    {
-    //        var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
-    //        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
-    //        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
-    //    }
-    //}
-
-    //public static async Task Main(string[] args)
-    //{
-    //    var playwright = await Playwright.CreateAsync();
-    //    var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-    //    {
-    //        Channel = "msedge", // Use the Edge browser
-    //        Headless = false
-    //    });
-    //    var page = await browser.NewPageAsync();
-    //    await page.GotoAsync("https://example.com");
-
-    //    // Inject JavaScript to use the Web Speech API for TTS
-    //    await page.EvaluateAsync(@"() => {
-    //        const utterance = new SpeechSynthesisUtterance('Hello, world!');
-    //        speechSynthesis.speak(utterance);
-    //    }");
-
-    //    // Wait for the speech to finish
-    //    await Task.Delay(5000);
-
-    //    await browser.CloseAsync();
-    //}
 }
