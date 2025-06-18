468f9af0a65385a669c14c889c109bf4715dc897
Andrey Tretyak <andrey.o.tretyak@gmail.com>
2025-06-17 19:39:59 -0700

    Use filescoped namespaces

--- src/AudiobookGenerator.Wpf/MainWindow.xaml.cs
+++ src/AudiobookGenerator.Wpf/MainWindow.xaml.cs
@@ -1,26 +1,27 @@
 ﻿using Microsoft.Extensions.Logging.Abstractions;
+
 using System.Windows;
+
 using YewCone.AudiobookGenerator.Core;
 using YewCone.AudiobookGenerator.Wpf.ViewModels;
 
-namespace YewCone.AudiobookGenerator.Wpf
+namespace YewCone.AudiobookGenerator.Wpf;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            IHtmlConverter converter = true
-                ? new HtmlAgilityPackHtmlConverter(new NullLogger<HtmlAgilityPackHtmlConverter>())
-                : new PlaywrightHtmlConverter(new NullLogger<PlaywrightHtmlConverter>());
+        IHtmlConverter converter = true
+            ? new HtmlAgilityPackHtmlConverter(new NullLogger<HtmlAgilityPackHtmlConverter>())
+            : new PlaywrightHtmlConverter(new NullLogger<PlaywrightHtmlConverter>());
 
-            var parser = new VersOneEpubBookParser(converter, new NullLogger<VersOneEpubBookParser>());
-            var synthesizer = new LocalAudioSynthesizer(new NullLogger<LocalAudioSynthesizer>());
+        var parser = new VersOneEpubBookParser(converter, new NullLogger<VersOneEpubBookParser>());
+        var synthesizer = new LocalAudioSynthesizer(new NullLogger<LocalAudioSynthesizer>());
 
-            DataContext = new AudiobookGeneratorViewModel(parser, synthesizer);
-            InitializeComponent();
-        }
+        DataContext = new AudiobookGeneratorViewModel(parser, synthesizer);
+        InitializeComponent();
     }
 }
